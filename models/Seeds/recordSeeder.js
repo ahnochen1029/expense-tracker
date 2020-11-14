96ab2f0f652b3000ad424c1dbaeb22cf97c16c61
Ahno Chen <ahnochen1029@gmail.com>
2020-11-14 16:09:44 +0800

    recordseeder

--- models/Seeds/recordSeeder.js
+++ models/Seeds/recordSeeder.js
@@ -1,49 +1,86 @@
-const Record = require('../record')
+const bcrypt = require('bcryptjs')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const db = require('../../config/mongoose')
 
+const Record = require('../record')
+const User = require('../user')
+
+const SEED_USER = {
+  name: 'user',
+  email: 'user@example.com',
+  password: '0000'
+}
+
 db.once('open', () => {
-  Record.create(
-    {
-      name: '鐵板燒',
-      category: '餐飲食品',
-      merchant: '好吃鐵板燒',
-      date: '2020-08-01',
-      amount: 350,
-      icon: 'fas fa-utensils'
-    },
-    {
-      name: '高鐵票',
-      category: '交通出行',
-      merchant: '台灣高鐵',
-      date: '2020-08-10',
-      amount: 700,
-      icon: 'fas fa-shuttle-van'
-    },
-    {
-      name: '房租',
-      category: '家居物業',
-      merchant: '其他',
-      date: '2020-08-20',
-      amount: 15000,
-      icon: 'fas fa-home'
-    },
-    {
-      name: '看棒球',
-      category: '休閒娛樂',
-      merchant: 'CPBL',
-      date: '2020-08-21',
-      amount: 500,
-      icon: 'fas fa-grin-beam'
-    },
-    {
-      name: '保單',
-      category: '其他',
-      merchant: '富邦人壽',
-      date: '2020-08-26',
-      amount: 2500,
-      icon: 'fas fa-pen'
-    },
-  )
-  console.log('mongodb_record connected!')
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash =>
+      User.create({
+        name: SEED_USER.name,
+        email: SEED_USER.email,
+        password: hash
+      })
+    )
+    .then(user => {
+      const userId = user._id
+      return Promise.all(Array.from(
+        { length: 1 },
+        (_, i) => Record.create(
+          {
+            name: '鐵板燒',
+            category: '餐飲食品',
+            merchant: '好吃鐵板燒',
+            date: '2020-08-01',
+            amount: 350,
+            icon: 'fas fa-utensils',
+            userId
+          },
+          {
+            name: '高鐵票',
+            category: '交通出行',
+            merchant: '台灣高鐵',
+            date: '2020-08-10',
+            amount: 700,
+            icon: 'fas fa-shuttle-van',
+            userId
+          },
+          {
+            name: '房租',
+            category: '家居物業',
+            merchant: '其他',
+            date: '2020-08-20',
+            amount: 15000,
+            icon: 'fas fa-home',
+            userId
+          },
+          {
+            name: '看棒球',
+            category: '休閒娛樂',
+            merchant: 'CPBL',
+            date: '2020-08-21',
+            amount: 500,
+            icon: 'fas fa-grin-beam',
+            userId
+          },
+          {
+            name: '保單',
+            category: '其他',
+            merchant: '富邦人壽',
+            date: '2020-08-26',
+            amount: 2500,
+            icon: 'fas fa-pen',
+            userId
+          },
+        )
+      ))
+    })
+    .then(() => {
+      console.log('done recordseeder.')
+      process.exit()
+    })
 })
