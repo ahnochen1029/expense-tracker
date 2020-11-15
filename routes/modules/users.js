31134d7550a584a5dc1ec0756a767dcf34f138c0
Ahno Chen <ahnochen1029@gmail.com>
2020-11-15 09:53:49 +0800

    logout

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -65,47 +65,11 @@ router.post('/register', (req, res, next) => {
 })
 
 
-// router.post('/register', (req, res, next) => {
-//   const { name, email, password, confirmPassword } = req.body
-//   const errors = []
-
-//   // handle errors situation
-//   if (!name || !email || !password || !confirmPassword) {
-//     errors.push({ message: 'All fields are required.' })
-//   }
-//   if (password.length < 8) {
-//     errors.push({ message: 'Password at least 8 characters minimum.' })
-//   }
-//   if (password !== confirmPassword) {
-//     errors.push({ message: 'Your password and confirmation password do not match.' })
-//   }
-//   if (errors.length) {
-//     return res.render('register', { errors, name, email, password, confirmPassword })
-//   }
-
-//   User.findOne({ email }).then(user => {
-//     if (user) {
-//       errors.push({ message: 'This email address is already registered.' })
-//       return res.render('register', { errors, name, email, password, confirmPassword })
-//     }
-//     return bcrypt.genSalt(10)
-//       .then(salt => bcrypt.hash(password, salt))
-//       .then(hash => User.create({ name, email, password: hash }))
-//       .then(() => next())
-//       .catch(err => console.log(err))
-//   })
-// }, passport.authenticate('local', {
-//   successRedirect: '/',
-//   failureRedirect: '/users/login',
-//   successFlash: true,
-//   failureFlash: true
-// }))
-
-// router.get('/logout', (req, res) => {
-//   req.logout()
-//   req.flash('success_msg', '你已經成功登出。')
-//   res.redirect('/users/login')
-// })
+router.get('/logout', (req, res) => {
+  req.logout()
+  req.flash('success_msg', '你已經成功登出。')
+  res.redirect('/users/login')
+})
 
 
 module.exports = router
